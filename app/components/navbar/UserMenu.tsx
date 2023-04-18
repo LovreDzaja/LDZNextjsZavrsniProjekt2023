4c817125718fe9600fd5e07473c53459db30fb51
you <you@example.com>
2023-04-18 22:06:07 +0200

    bug fix

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -6,12 +6,12 @@ import MenuItem from './MenuItem';
 import useRegisterModal from '@/app/hooks/useRegisterModal';
 import useLoginModal from '@/app/hooks/useLoginModals';
 import { signOut } from 'next-auth/react';
-import { User } from '@prisma/client';
 import useRentModal from '@/app/hooks/useRentModal';
 import { useRouter } from 'next/navigation';
+import { SafeUser } from '@/app/types';
 
 interface UserMenuProps{
-    currentUser?: User | null;
+    currentUser?: SafeUser | null;
 }
 
 const UserMenu: React.FC<UserMenuProps> = ({currentUser}) => {
