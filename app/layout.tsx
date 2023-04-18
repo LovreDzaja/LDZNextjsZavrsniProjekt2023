9012ab649fdcc285e4a1f3f7325409ba013a0dac
you <you@example.com>
2023-04-18 21:57:27 +0200

    bug fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default async function RootLayout({
           <SearchModal/>
           <RegisterModal/>
           <LoginModal/>
-          <Navbar currentUser={currentUser}/>
+          <Navbar currentUser={currentUser} />
         </ClientOnly>
         <div className='pb-20 pt-28'>
           {children}
